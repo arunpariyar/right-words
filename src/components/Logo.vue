7d3038058953968ce11f97939bdbbd3178c4e150
Arun Pariyar <arun.pariyar@gmail.com>
2022-08-23 12:04:12 +0200

    Base css style

---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ logo }}
+  </div>
+</template>
+<script>
+export default {
+  name: "Logo",
+  data() {
+    return {
+      logo: "Right Words",
+    };
+  },
+};
+</script>
